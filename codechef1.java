af189b0c23ec510fd5d7f53e098fc09fb6af2e1d
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2019-04-24 18:32:45 +0530

    Add files via upload

---
+++ codechef1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+class Demo{
+	public static void main(String arv[])
+{
+		
+		Scanner scan =new Scanner(System.in);
+		int n;
+		while ((n = scan.nextInt()) != 42){
+		System.out.println(n);	
+}
+
+	} 
+}
